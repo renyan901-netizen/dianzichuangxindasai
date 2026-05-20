bd236d902acff2bdd0decae5afc7024b33f36858
Your Name <youremail@example.com>
2026-05-20 22:44:42 +0800

    V3: Deploy V3 gesture control to root level and clean up local build conflict directories

--- V3/Hardware copy/servo.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SERVO_H
-#define __SERVO_H
-
-#include "ti_msp_dl_config.h"
-
-void Servo_Init(void);
-void Servo_SetAngle(uint8_t channel, float angle);
-
-#endif
--- V3/Hardware/motor.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MOTOR_H__
-#define __MOTOR_H__
-
-#include "ti_msp_dl_config.h"
-
-void Motor_Init(void);
-void Motor_SetSpeed(int8_t left_speed, int8_t right_speed);
-
-#endif
